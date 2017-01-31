62e3e22dead84dcad89375fab42b0bea2dee4b9f
Chyld Medford <chyld.medford@gmail.com>
2017-01-31 16:14:40 +0530

    ready

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  target: 'electron',
+  entry: './src/client/index.js',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, 'out/client')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
